7a8bb1ef51ac959298fa79b156ae3f78c2b2f076
wolf1312 <wolfgang.palfinger@gmx.at>
2019-05-22 15:23:19 +0200

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,9 +1,14 @@
 window.jstools = {
-	
-	
-	alert : function(){
-	   alert('X');
-	}
+		
+	getTable:function(rows,columns,appendTo){
+		var tab = $('<table/>').appendTo(appendTo);
+		var tds = [];
+		for(let i=0; i < rows; i++){
+			var row = $('<tr/>').appendTo(tab);
+			tds[i] = [];
+			for(let j=0; j < columns; j++) tds[i][j] = $('<td/>').appendTo(row);
+		}
+		return tds;
+	}   
 		
 };
-debugger;
